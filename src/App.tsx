a77642929f9d88100eaf204d3194cfebc7e48ee6
Antoine BERNIER <antoine.bernier@gmail.com>
2023-04-05 22:23:16 +0200

    useCubeCamera

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import * as THREE from "three";
-import { useEffect, useState, useMemo, useLayoutEffect } from "react";
+import {
+  useEffect,
+  useState,
+  useMemo,
+  useLayoutEffect,
+  useCallback,
+} from "react";
 import styled from "@emotion/styled";
-import { Canvas, useThree } from "@react-three/fiber";
-import { useKeyboardControls, useGLTF } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  useKeyboardControls,
+  useGLTF,
+  CubeCamera,
+  useFBO,
+} from "@react-three/drei";
 import { Physics, Debug } from "@react-three/rapier";
 import { XR, Controllers, Hands, VRButton, Interactive } from "@react-three/xr";
 
@@ -76,6 +87,21 @@ function Scene() {
   }, [gltf.nodes]);
   console.log("OOI", OOI);
 
+  const { fbo, camera, update } = useCubeCamera({ resolution: 256 });
+
+  useFrame(() => {
+    OOI.boule.visible = false; // hide sphere before taking cube photos
+    camera.position.copy(OOI.boule.getWorldPosition(new THREE.Vector3()));
+    update(); // 📸 shoot cubeCamera
+    OOI.boule.visible = true; // reveal back the sphere
+  });
+
+  useEffect(() => {
+    (OOI.boule as THREE.Mesh).material = new THREE.MeshBasicMaterial({
+      envMap: fbo.texture,
+    });
+  }, [OOI.boule, fbo.texture]);
+
   useLayoutEffect(() => {
     gltf.scene.traverse((n) => {
       console.log(n);
@@ -92,4 +118,35 @@ function Scene() {
       <Ground />
     </>
   );
+}
+
+function useCubeCamera({
+  resolution = 256,
+  near = 0.1,
+  far = 1000,
+}: {
+  resolution?: number;
+  near?: number;
+  far?: number;
+}) {
+  const gl = useThree((state) => state.gl);
+  const scene = useThree((state) => state.scene);
+
+  const fbo = useMemo(() => {
+    const fbo = new THREE.WebGLCubeRenderTarget(resolution);
+    fbo.texture.encoding = gl.outputEncoding;
+    fbo.texture.type = THREE.HalfFloatType;
+    return fbo;
+  }, [resolution, gl.outputEncoding]);
+
+  const camera = useMemo(
+    () => new THREE.CubeCamera(near, far, fbo),
+    [near, far, fbo]
+  );
+
+  const update = useCallback(() => {
+    camera.update(gl, scene);
+  }, [gl, scene, camera]);
+
+  return { fbo, camera, update };
 }
